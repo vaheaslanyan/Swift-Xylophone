e9a09612fc396f19ef72bf320ea941f7c62904a3
Vahe Aslanyan <vahe.sde@gmail.com>
2020-06-18 14:49:43 -0700

    initial commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Vahe Aslanyan on 3/15/2020
+//  Copyright © 2020 Vahe Aslanyan. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        print("start")
+            
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+            print("end")
+         }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
